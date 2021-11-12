138f3d2fc65ac9fff8148d45433b4cc3b656789a
Maikon Muniz <maikon.muni@yahoo.com.br>
2021-11-11 23:14:12 -0300

    atualizaçao de pet

--- back_end/routes/petRoutes.js
+++ back_end/routes/petRoutes.js
@@ -14,4 +14,7 @@ router.get('/myadoptions', verifyToken, PetController.getAllUserAdoptions)
 router.get('/:id', PetController.getPetById)
 router.delete('/:id', verifyToken, PetController.removePetById)
 
+router.patch('/:id', verifyToken, imageUpload.array('images'), PetController.updatePet)
+
+
 module.exports = router
